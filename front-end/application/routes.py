64d4407558d118fa7dd1f471f93a3934ff2c6d9f
qasim <qasimharoon@outlook.com>
2022-04-05 09:59:25 +0000

    edited front-end routes

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -1,7 +1,7 @@
 from application import app, db
 from application.models import Make_meal
 import requests
-from flask import render_template
+from flask import render_template, url_for, requests
 
 
 @app.route('/')
@@ -11,9 +11,13 @@ def index():
     price = requests.get('http://service_3:5000/post-meal')
     db.session.add(Make_meal(main=main.json()["main"], side=side.json()["side"], price_main=price_main.json()["price_main"], price_side=price_side.json()["price_side"], total_price=total_price.json()["total_price"]))
     db.session.commit()
-    meals = Make_meal.query.all() 
-    return render_template('index.html', meals = meals)
+    meals = Make_meal.query.all()
+    last5meals = Make_meal.query.order_by(Make_meal.id.desc()).limit(5).all() 
+    return render_template('index.html', main_dish = main_dish.json()["main_dish"], side_dish = side_dish.json()["side_dish"], price = price.json()["price"], meals = meals, last5meals = last5meals)
 
 
 
-    ## look into how to add the pricing 
+@app.route('/history', methods=['GET'])
+def history():
+    meal_history = Make_meal.query.all
+    return render_template("history.html", meal_history = meal_history) 
