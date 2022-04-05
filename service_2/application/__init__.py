b89146794ddf3f05f6e5896b8b074919a2a8206c
qasim <qasimharoon@outlook.com>
2022-04-05 13:56:47 +0000

    edited init.py

--- service_2/application/__init__.py
+++ service_2/application/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from os import getenv
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/meal-gen_db"
-db = SQLAlchemy(app)
 
 from application import routes
