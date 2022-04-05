6f5931e555fa1381ff9a4e6cc3f7e49108614d77
qasim <qasimharoon@outlook.com>
2022-04-05 09:16:39 +0000

    deploy.sh edited

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 export MYSQL_ROOT_PASSWORD
-docker stack deploy --compose-file docker-compose.yaml animal-noises-stack
+docker stack deploy --compose-file docker-compose.yaml meal-gen-stack
 docker service update --replicas 3 meal-gen-stack_front-end
