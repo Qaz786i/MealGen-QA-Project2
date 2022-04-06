19427917f421e72f75d7af7fd0aea0a0e8d81b20
qasim <qasimharoon@outlook.com>
2022-04-06 14:57:14 +0000

    db name

--- front-end/application/__init__.py
+++ front-end/application/__init__.py
@@ -3,7 +3,7 @@ from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/meal-gen_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{getenv('MYSQL_ROOT_PASSWORD')}@mysql:3306/mealgen-db"
 db = SQLAlchemy(app)
 
 from application import routes
