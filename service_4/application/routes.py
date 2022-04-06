7f6ab33ce1e791b9a3a3cfb9d8baca5d1a9badd6
qasim <qasimharoon@outlook.com>
2022-04-06 13:40:40 +0000

    routes edited

--- service_4/application/routes.py
+++ service_4/application/routes.py
@@ -3,7 +3,7 @@ from flask import request, jsonify
 
 prices = {'Tuna Mayo Baguette': 3, 'Prawn Mayo Sandwich': 1, 'Chicken Tikka Curry and Biryani': 5, 'Ploughmans Cheese Sandwich': 2, 'Subway Footlong sub': 4, 'KFC Ricebox': 7, 'Prawn Cocktail Walkers': 1, 'Kettle Chips Sea Salt & Malt Vinegar': 2, 'Sensations Sweet Chili': 1, 'Fruit Bowl': 2, 'Pineapple Chunks': 2, 'Apple Slices': 1}
 
-@app.route('/post-meal', methods=['GET','POST'])
+@app.route('/post-meal', methods=['POST'])
 def post_meal():
     data = request.get_json()
     main = data["main"]
