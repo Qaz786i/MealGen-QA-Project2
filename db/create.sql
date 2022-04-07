d2395ce3390e2a23901d81c0b64bd754265be3ca
qasim <qasimharoon@outlook.com>
2022-04-07 10:46:51 +0000

    increased varchar length for side

--- db/create.sql
+++ db/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS make_meal (
     id INT PRIMARY KEY AUTO_INCREMENT,
     main VARCHAR(100) NOT NULL,
-    side VARCHAR(30) NOT NULL,
+    side VARCHAR(100) NOT NULL,
     price_main INT NOT NULL,
     price_side INT NOT NULL,
     total_price INT NOT NULL
--- front-end/application/models.py
+++ front-end/application/models.py
@@ -3,7 +3,7 @@ from application import db
 class Make_meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     main = db.Column(db.String(100), nullable=False)
-    side = db.Column(db.String(30), nullable=False)
+    side = db.Column(db.String(100), nullable=False)
     price_main = db.Column(db.Integer)
     price_side = db.Column(db.Integer)
     total_price = db.Column(db.Integer)
