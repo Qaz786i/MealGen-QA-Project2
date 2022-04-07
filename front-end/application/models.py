e00cb7cb5faa7aee94bb149bbff6a0f26ad31432
qasim <qasimharoon@outlook.com>
2022-04-07 09:50:44 +0000

    edited models

--- front-end/application/models.py
+++ front-end/application/models.py
@@ -2,10 +2,10 @@ from application import db
 
 class Make_meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    main_dish = db.Column(db.String(30), nullable=False)
-    side_dish = db.Column(db.String(30), nullable=False)
+    main = db.Column(db.String(30), nullable=False)
+    side = db.Column(db.String(30), nullable=False)
     price_main = db.Column(db.Integer)
     price_side = db.Column(db.Integer)
     total_price = db.Column(db.Integer)
     def __str__(self):
-        return f"Mains: {self.main_dish} {self.price_main}, Sides: {self.side_dish} {self.price_side}, Total Price: {self.price}"
+        return f"Mains: {self.main} {self.price_main}, Sides: {self.side} {self.price_side}, Total Price: {self.price}"
