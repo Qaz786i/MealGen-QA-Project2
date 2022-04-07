dcc9b17c6c8c73c417b6bc3307709164d47af3b4
qasim <qasimharoon@outlook.com>
2022-04-07 10:24:54 +0000

    edited sql

--- db/create.sql
+++ db/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS make_meal (
     pk INT PRIMARY KEY AUTO_INCREMENT,
-    main_dish VARCHAR(30) NOT NULL,
-    side_dish VARCHAR(30) NOT NULL,
+    main VARCHAR(30) NOT NULL,
+    side VARCHAR(30) NOT NULL,
     price_main INT NOT NULL,
     price_side INT NOT NULL,
     total_price INT NOT NULL
